bc5ff1ec0876df1b56ed41e73c8de31bbc84bdc0
Henrik Kulterud <henrik@mixdesign.no>
2021-04-22 23:29:34 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -27,5 +27,16 @@ docker pull hkulterud/dockerhub:g7mp3_image
 docker container run -d --cap-drop=setfcap --cpu-shares 512 -m 512m -it -p 8000:80 --name REST_API hkulterud/dockerhub:g7mp3_image
 docker container run -d --cap-drop=setfcap --cpu-shares 512 -m 512m -it -p 8080:80 --name WEB_INTERFACE hkulterud/dockerhub:g7mp3_image
 ## 
-## Containere kjører med 512 cpu-shares og limit på 512MB ram, samt dropper capabilities setfcap
-##
+## Containere kjører med 512 cpu-shares og limit på 512MB ram (cgroups), samt dropper capabilities setfcap
+## Her kan man kjøre --cap-drop=all når man er ferdig å jobbe i containeren
+
+# Eventuelt kopiere filer
+docker cp rest.sh  g7alpine1:/usr/local/apache2/cgi-bin
+docker cp editor.sh  g7alpine2:/usr/local/apache2/cgi-bin
+
+docker cp httpd.conf  g7alpine1:/usr/local/apache2/conf
+docker cp httpd.conf  g7alpine2:/usr/local/apache2/conf
+
+# Sette rettigheter hvis det ikke er gjort allerede
+# chmod 755 cgi-bin/editor.sh
+# chmod 755 cgi-bin/rest.sh
